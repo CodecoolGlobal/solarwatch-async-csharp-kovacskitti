40f9652bbef50a7d3b0dd5d9c569b549917f193b
kovacskitti <kovacskitti001@gmail.com>
2023-11-28 19:11:00 +0100

    feat: add SolarWatch model

---
+++ SolarWatch/SolarWatch/Model/SolarWatch.cs
@@ -0,0 +1,12 @@
+namespace SolarWatch.Modell;
+
+public class SolarWatch
+{
+    public string Location { get; set; }
+    public string Date { get; set; }
+
+    public string Sunrise { get; set; }
+    
+    public string Sunset { get; set; }
+    
+}
