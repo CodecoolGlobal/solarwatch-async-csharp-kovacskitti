39786ba8f896f30996a78fb016b0fdaf0a54b2e0
kovacskitti <kovacskitti001@gmail.com>
2023-11-29 09:17:03 +0100

    feat: add IGeocodingDataProvider class

---
+++ SolarWatch/SolarWatch/Service/GeocodingApi.cs
@@ -0,0 +1,24 @@
+using System.Net;
+
+namespace SolarWatch.Service;
+
+public class GeocodingApi : IGeocodingDataProvider
+{
+    private readonly ILogger<GeocodingApi> _logger;
+
+    public GeocodingApi(ILogger<GeocodingApi> logger)
+    {
+        _logger = logger;
+    }
+    
+    public string GetCurrent(string location)
+    {
+        var ApiKey = "a3c473aaa1de98a4b8c1bbb6547e0b71";
+        var url = $"http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=5&appid={ApiKey}";
+        var client = new WebClient();
+        
+        _logger.LogInformation(message:"Calling OpenWeather API with url: {url}", url);
+        Console.WriteLine(client.DownloadString(url));
+        return client.DownloadString(url);
+    }
+}
