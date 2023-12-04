f8aeb6dc0fe24bab4bfa7a1feef259247b6eca01
kovacskitti <kovacskitti001@gmail.com>
2023-12-04 09:21:07 +0100

    feat: add AppDbContext

--- SolarWatch/SolarWatch/Service/AppDbContext.cs
+++ SolarWatch/SolarWatch/Service/AppDbContext.cs
@@ -5,7 +5,8 @@ public class AppDbContext : DbContext
 {
     public DbSet<City> Cities { get; set; }
     public DbSet<SunriseSunset> SunTimes { get; set; }
-    
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+}
