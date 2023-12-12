ac50bf54afa166f0ed9a04cfc6cfb744a99ba230
kovacskitti <kovacskitti001@gmail.com>
2023-12-12 13:16:44 +0100

    reafactor: using JsonSerializer, dbContext

--- SolarWatch/SolarWatch/Service/JsonProcessorGeocodingApi.cs
+++ SolarWatch/SolarWatch/Service/JsonProcessorGeocodingApi.cs
@@ -1,3 +1,4 @@
+using System.Data.Entity;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using SolarWatch.Model;
@@ -7,17 +8,44 @@ namespace SolarWatch.Service;
 
 public class JsonProcessorGeocodingApi:IJsonProcessorToGeocoding
 {
-    public Coordinate Process(string data)
+    private readonly AppDbContext _dbContext;
+
+    public JsonProcessorGeocodingApi(AppDbContext dbContext)
     {
-        var result = new Dictionary<string, float>();
-        var json = JsonDocument.Parse(data);
-        var firstElement = json.RootElement.EnumerateArray().First();
-        var lat = firstElement.GetProperty("lat");
-        var lon = firstElement.GetProperty("lon");
-        lat.TryGetSingle(out float latFloat);
-        lon.TryGetSingle(out float lonFloat);
-        var currentCoordinate = new Coordinate(latFloat,lonFloat) {};
-        return currentCoordinate;
+        _dbContext = dbContext;
+    }
+    public City Process(string data)
+    {
+        
+        var cityList = JsonSerializer.Deserialize<IList<CityData>>(data, new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        });
+        
+        
+        var firstCityData = cityList[0];
+        var currentCity = new City()
+        {
+            Name = firstCityData.Name,
+            Coordinate = new Coordinate(
+                firstCityData.Lat, firstCityData.Lon),
+            State = firstCityData.State,
+            Country = firstCityData.Country
+        };
+        
+        _dbContext.Cities.Add(currentCity);
+        Console.WriteLine(currentCity.State);
+        Console.WriteLine(firstCityData.State);
+        _dbContext.SaveChanges();
+        // var json = JsonDocument.Parse(data);
+        // var firstElement = json.RootElement.EnumerateArray().First();
+        // var lat = firstElement.GetProperty("lat");
+        // var lon = firstElement.GetProperty("lon");
+        // lat.TryGetSingle(out float latFloat);
+        // lon.TryGetSingle(out float lonFloat);
+        // var currentCoordinate = new Coordinate(latFloat,lonFloat) {};
+        // return currentCoordinate;
+        return currentCity;
     }
     
   
