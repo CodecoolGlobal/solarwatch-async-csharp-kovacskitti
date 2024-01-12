451ab9c37e9af143d616664c0f9627f5f49e4133
kovacskitti <kovacskitti001@gmail.com>
2024-01-12 12:57:40 +0100

    docs: add Registration page

---
+++ SolarWatch_Frontend/src/pages/Registration.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    const registrationData = {
+      UserName: username,
+      Email: email,
+      Password: password,
+      };
+
+    try {
+      const response = await fetch("/api/Auth/Register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registrationData),
+      });
+
+      if (!response.ok) {
+        console.error("Registration failed:", response.statusText);
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Registration successful");
+      console.log("User Email:", data.email);
+      console.log("User Token:", data.token);
+    } catch (error) {
+      console.error("Error during registration:", error);
+    }
+  };
+
+  return (
+    <div className="registration-container">
+      <h2>Registration</h2>
+      <form onSubmit={handleRegistration}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
