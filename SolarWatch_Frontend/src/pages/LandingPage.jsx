fcc30bbe56eb5edf5085ec890cc03e6786624867
kovacskitti <kovacskitti001@gmail.com>
2024-01-12 11:00:47 +0100

    docs: add Landing page

---
+++ SolarWatch_Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import "../index.css";
+
+const LandingPage = ({ children }) => {
+ 
+  return (
+    <div className="landing-page">
+      <nav className="nav-container">
+        <ul className="nav-list">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/registration">Registration</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+     <Outlet />
+    </div>
+  );
+};
+
+export default LandingPage;
