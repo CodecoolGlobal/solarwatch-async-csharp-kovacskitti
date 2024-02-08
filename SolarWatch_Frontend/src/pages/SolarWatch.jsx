01b33afd31401ffbff5ba84793440b82a0f98a9f
kovacskitti <kovacskitti001@gmail.com>
2024-02-08 13:07:51 +0100

    refact: change options of chart

--- SolarWatch_Frontend/src/pages/SolarWatch.jsx
+++ SolarWatch_Frontend/src/pages/SolarWatch.jsx
@@ -135,10 +135,11 @@ const SolarWatch = () => {
                     const formattedTime = `${String(hours).padStart(
                       2,
                       "0"
-                    )}:${String(minutes).padStart(2, "0")}:00`;
+                    )}:${String(minutes).padStart(2, "0")}`;
                     return formattedTime;
                   },
                   color: "black",
+                  stepSize:1,
                 },
               },
             },
