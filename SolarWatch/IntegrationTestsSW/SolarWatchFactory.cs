ad254fad7320813bbbd408959af06287ffc135b7
kovacskitti <kovacskitti001@gmail.com>
2024-03-01 19:48:42 +0100

    docs: remove wrong tests

--- SolarWatch/IntegrationTestsSW/SolarWatchFactory.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-
-namespace SolarWatch.IntegrationTestsSW;
-
-public class SolarWatchFactory : WebApplicationFactory<Program>
-{
-    
-}
