13b900f9f9a81ce381e53b640d7c9c9c4d694ce1
kovacskitti <kovacskitti001@gmail.com>
2024-03-04 11:13:31 +0100

    style: add table to DOM

--- SolarWatch_Frontend/src/pages/Profile.jsx
+++ SolarWatch_Frontend/src/pages/Profile.jsx
@@ -4,52 +4,63 @@ const Profile = () => {
   const [favouriteCities, setFavouriteCities] = useState([]);
   const email = localStorage.getItem("userEmail");
 
-     useEffect(() => {
-       const fetchFavouriteCities = async () => {
-         if (email) {
-           try {
-             const response = await fetch(
-               `http://localhost:5186/Auth/GetProfileData/${email}`,
-               {
-                 method: "GET",
-                 headers: {
-                   "Content-Type": "application/json",
-                   Authorization: `Bearer ${localStorage.getItem(
-                     "accessToken"
-                   )}`,
-                 },
-               }
-             );
+  useEffect(() => {
+    const fetchFavouriteCities = async () => {
+      if (email) {
+        try {
+          const response = await fetch(
+            `http://localhost:5186/Auth/GetProfileData/${email}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+              },
+            }
+          );
 
-             if (!response.ok) {
-               console.error("Get user data failed:", response.statusText);
-               return;
-             }
+          if (!response.ok) {
+            console.error("Get user data failed:", response.statusText);
+            return;
+          }
 
-             const data = await response.json();
-             setFavouriteCities(data);
-             console.log("Profile data ok");
-           } catch (error) {
-             console.error("Error during profile data loading:", error);
-           }
-         }
-       };
+          const data = await response.json();
+          setFavouriteCities(data);
+          console.log("Profile data ok");
+        } catch (error) {
+          console.error("Error during profile data loading:", error);
+        }
+      }
+    };
 
-       fetchFavouriteCities();
-     }, [email]);
+    fetchFavouriteCities();
+  }, [email]);
 
   return (
     <div className="profile-container">
       <h2>Profile</h2>
-      <p>Username: {localStorage.userName}</p>
-      <p>email: {localStorage.userEmail}</p>
-      <ul>
-        favourite city / cities:
-        {favouriteCities.map((city) => (
-          <li>{city}</li>
-        ))}
-      </ul>
-      <p></p>
+      <table className="profile-table">
+        <tbody>
+          <tr>
+            <td>Username:</td>
+            <td>{localStorage.userName}</td>
+          </tr>
+          <tr>
+            <td>email:</td>
+            <td>{localStorage.userEmail} </td>
+          </tr>
+          <tr>
+            <td> favourite city / cities:</td>
+            <td>
+              <ul>
+                {favouriteCities.map((city) => (
+                  <li>{city}</li>
+                ))}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
